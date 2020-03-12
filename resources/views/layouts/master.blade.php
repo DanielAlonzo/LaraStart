952072f41f50cc805cd811556e60ba5bddfbbeea
Daniel <dani3plea@gmail.com>
2020-03-12 15:23:59 -0600

    12/03/2020 15:23

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,11 +10,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <title>AdminLTE 3 | Starter</title>
-
+ <!-- CSRF Token -->
+ <meta name="csrf-token" content="{{ csrf_token() }}">
  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -73,17 +74,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                <li class="nav-item">
-            <a href="#" class="nav-link">
+            <router-link to="/dashboard" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
                 
               </p>
-            </a>
+            </router-link>
           </li>
           
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-cog"></i>
               <p>
                 Mantenimiento
@@ -92,7 +93,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Active Page</p>
                 </a>
@@ -106,13 +107,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </ul>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <router-link to="/perfil" class="nav-link">
               <i class="nav-icon fas fa-user"></i>
               <p>
                 Perfil
                 
               </p>
-            </a>
+            </router-link>
           </li>
 
           <li class="nav-item">
@@ -140,7 +141,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-       
+      <router-view></router-view>
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
